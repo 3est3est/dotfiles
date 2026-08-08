998de0c3d15db6be67acefae003b01978b9ff7df
3est3est <kanompang450@gmail.com>
2026-08-09 02:47:06 +0700

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.updatetime = 300
+
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
+  command = "checktime",
+})
